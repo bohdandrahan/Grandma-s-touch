99703fd2d62e03f00deeaedd7c372e0f75079f8c
Bohdan <bohdan.drahan@gmail.com>
2020-04-10 15:39:13 -0400

    edit components, creating recipe models

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe('TestRec','This is a test','https://p2.piqsels.com/preview/488/632/975/tomato-mozzarella-food-recipe-meal.jpg'),    new Recipe('TestRec','This is a test','https://p2.piqsels.com/preview/488/632/975/tomato-mozzarella-food-recipe-meal.jpg'),
+    new Recipe('TestRec','This is a test','https://p2.piqsels.com/preview/488/632/975/tomato-mozzarella-food-recipe-meal.jpg'),
+    new Recipe('TestRec','This is a test','https://p2.piqsels.com/preview/488/632/975/tomato-mozzarella-food-recipe-meal.jpg'),
+    new Recipe('TestRec','This is a test','https://p2.piqsels.com/preview/488/632/975/tomato-mozzarella-food-recipe-meal.jpg')];
 
+
+
+
+  constructor() { }
   ngOnInit(): void {
   }
-
 }
